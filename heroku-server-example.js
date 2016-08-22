b058fa864ddbe1a9474f34e6ed71d08bacf1fc44
Danielle Closs <dcloss@arbor.net>
2016-08-22 19:33:01 -0400

    fixed eslint errors, changed single quotes to double quotes

--- heroku-server-example.js
+++ heroku-server-example.js
@@ -1,14 +1,15 @@
-var app = require('express')();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+var app = require("express")();
+var server = require("http").createServer(app);
+var io = require("socket.io")(server);
 
-app.set('port', (process.env.PORT || 3000));
+app.set("port", (process.env.PORT || 3000));
 
-app.get('/ece926d8c0356205276a45266d361161', function(request, response) {
+app.get("/ece926d8c0356205276a45266d361161", function(request, response) {
     response.send("OK");
-    io.emit('action');
+    io.emit("action");
 });
 
-server.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+server.listen(app.get("port"), function() {
+  console.log("Node app is running on port", app.get("port"));
 });
+
