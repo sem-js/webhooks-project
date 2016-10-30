b3c47180ce603c1ebd1c24642d3835a5108933ee
BrianGenisio <briangenisio@gmail.com>
2016-10-29 22:03:44 -0400

    create new hooks

--- webhooks-app/src/actions/hook-actions.js
+++ webhooks-app/src/actions/hook-actions.js
@@ -4,18 +4,32 @@ import {dataClient} from '../data-client.js';
 import appState from '../state/app-state';
 
 const fetchHooks = action(() => {
-    dataClient.query(`
-    query {
+    const query = `query {
         hookHandlers {
             id, name
         }
-    }`).then(response => {
+    }`;
+
+    dataClient.query(query).then(response => {
         action(() => {
             response.hookHandlers.forEach(hook => appState.hooks.push(observable(hook)));
         })();
     });
 });
 
+const createHook = action(name => {
+    const mutation = `($name: String) {
+        newHook: createHookHandler(name: $name) {
+            id, name
+        }
+    }`;
+
+    dataClient.mutate(mutation, {name}).then(response => {
+        appState.hooks.push(observable(response.newHook));
+    });
+});
+
 export {
-    fetchHooks
+    fetchHooks,
+    createHook
 };
--- webhooks-app/src/hooks/hook-list-view.js
+++ webhooks-app/src/hooks/hook-list-view.js
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
+import {extendObservable} from "mobx";
 import {observer} from "mobx-react";
 import HookView from "./hook-view";
+import {createHook} from "../actions/hook-actions";
 
 class HookListView extends Component {
+    constructor() {
+        super();
+
+        extendObservable(this, {
+            newHookName: ''
+        });
+    }
+
     render() {
         return <div>
         {this.props.hooks.map(hook => {
@@ -10,6 +20,10 @@ class HookListView extends Component {
             return <HookView hook={hook} key={hook.id} />
         }
         )}
+            <div>
+                <input onChange={evt => this.newHookName = evt.target.value} />
+                <button onClick={() => createHook(this.newHookName)}>Create Test</button>
+            </div>
         </div>;
     }
 };
