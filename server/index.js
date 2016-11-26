2c5bac36d3de96367faf889990af4be6e6081a87
BrianGenisio <briangenisio@gmail.com>
2016-11-26 11:32:26 -0500

    bump?

--- server/index.js
+++ server/index.js
@@ -6,4 +6,5 @@ var app = express();
 app.use('/api/graphql', gqlMiddleware);
 app.use(express.static('public'));
 
+
 app.listen(4000, () => console.log('Now browse to localhost:4000/api/graphql'));
