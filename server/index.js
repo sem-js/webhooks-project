dbe7cbcaa093e46b3981c6807632eb6b44d0e134
BrianGenisio <briangenisio@gmail.com>
2016-11-26 11:39:11 -0500

    don't forget the port fowarding

--- server/index.js
+++ server/index.js
@@ -7,4 +7,4 @@ app.use('/api/graphql', gqlMiddleware);
 app.use(express.static('public'));
 
 
-app.listen(4000, () => console.log('Now browse to localhost:4000/api/graphql'));
+app.listen((process.env.PORT || 4000), () => console.log('Now browse to localhost:4000/api/graphql'));
